a8ffd71d43b4d7098245b0dbb82ce7a026e082bb
moogoofoo <tomdusk808@gmail.com>
2025-08-13 08:59:14 -0700

    fix openhands startup script

--- openhands.sh
+++ openhands.sh
@@ -1,10 +1,12 @@
 docker run -it --rm --pull=always \
     -e SANDBOX_RUNTIME_CONTAINER_IMAGE=docker.all-hands.dev/all-hands-ai/runtime:main-nikolaik \
     -e LOG_ALL_EVENTS=true \
-    #-e LLM_TEMPERATURE=1.0 \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v ~/.openhands:/.openhands \
+    --env-file .env \
     -p 3000:3000 \
     --add-host host.docker.internal:host-gateway \
     --name openhands-app \
     docker.all-hands.dev/all-hands-ai/openhands:main
+
+#-e LLM_TEMPERATURE=1.0 \
